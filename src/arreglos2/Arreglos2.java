239b971deb21f39fe7817f53e820c2a355fe2739
Reinel <rjtangarifer@unal.edu.co>
2021-07-06 10:21:43 -0500

    Se crea el proyecto

---
+++ src/arreglos2/Arreglos2.java
@@ -0,0 +1,41 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RT
+ */
+public class Arreglos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Scanner entrada= new Scanner(System.in);
+        int [][] cajonDeCajones= new int [2][2];
+        for (int fila=0; fila<2; fila++){
+            for (int columna=0; columna<2; columna++){
+                System.out.println("Digite el numero a guardar en: "+"["+fila+"]"+"["+columna+"]");
+                cajonDeCajones[fila][columna]=entrada.nextInt();
+                
+            }
+        }
+        
+        for (int fila=0; fila<2; fila++){
+            for (int columna=0; columna<2; columna++){
+                System.out.print(cajonDeCajones[fila][columna]+ " ");
+  
+            }
+            System.out.println("");
+        }*/
+        
+        Scanner entrada= new Scanner(System.in);
+    }
+    
+}
